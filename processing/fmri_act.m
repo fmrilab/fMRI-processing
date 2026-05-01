8743b3bbc57829b052ff989e4ae56520825698fa
YongliHe23 <yonglihe@umich.edu>
2026-05-01 11:33:02 -0400

    Revert "Use conv(,same) to avoid "deadtime" at the beginning of the ref"
    This reverts commit d94de30abd486657d3962fcbdecee1d234a77d08.
    
    "deadtime" is actually physically realistic, keep it.

--- processing/fmri_act.m
+++ processing/fmri_act.m
@@ -45,8 +45,8 @@ function ref = fmri_act(t, t_on, t_off, t_del, varargin)
     stim = 1*(mod(t - t_del, t_on + t_off) >= t_off).*(t >= t_del);
     
     % convolve to create activation waveform
-    ref = conv(stim(:)',hrf(:).',"same");
-    
+    ref = conv(hrf(:).',stim(:).');
+    ref = ref(1:nt);
 
 end
 
