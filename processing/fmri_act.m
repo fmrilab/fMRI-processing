f770d4e1233159e719a48be9e2bae9c10a2833ff
YongliHe23 <yonglihe@umich.edu>
2026-05-01 11:39:40 -0400

    change arguments order in conv()

--- processing/fmri_act.m
+++ processing/fmri_act.m
@@ -45,7 +45,7 @@ function ref = fmri_act(t, t_on, t_off, t_del, varargin)
     stim = 1*(mod(t - t_del, t_on + t_off) >= t_off).*(t >= t_del);
     
     % convolve to create activation waveform
-    ref = conv(hrf(:).',stim(:).');
+    ref = conv(stim(:).',hrf(:).');
     ref = ref(1:nt);
 
 end
